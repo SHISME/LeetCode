5176b4dd6693a3c0b142b6e11e0b0e7e95450816
sh <475784041@qq.com>
2019-01-15 23:16:37 +0800

    Search in Rotated Sorted Array

--- src/33/index.js
+++ src/33/index.js
@@ -1,34 +1,40 @@
 /**
- * 思路
  * 
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
  */
 var search = function(nums, target) {
-    let low = 0;
-    let high = nums.length - 1;
-    const rotated_index = nums.findIndex((num, index) => {
-        return index > 1 && num < nums[index - 1];
-    });
-    if (nums[rotated_inde] === target) {
-        return rotated_index;
-    }
-    if (nums[rotated_index] > target) {
+    function binarySearch(left, right) {
+        while (left <= right) {
+            mid = Math.floor((left + right) / 2);
+            if (nums[mid] === target) {
+                return mid;
+            }
+            if (nums[mid] > target) {
+                right = mid - 1;
+                continue;
+            }
+            left = mid + 1;
+        }
         return -1;
     }
-    
-
-    // while (low < high) {
-    //     const middle = Math.floor((high + low) / 2);
-    //     if (target === nums[middle]) {
-    //         return middle;
-    //     }
-    // }
-    return rotated_index;
+    let rotated_index = nums.findIndex((num, index) => {
+        return index >= 1 && num < nums[index - 1];
+    });
+    if (rotated_index === -1) {
+        rotated_index = 0;
+    }
+    let result = binarySearch(0, rotated_index - 1);
+    if (result === -1) {
+        result = binarySearch(rotated_index, nums.length - 1);
+    }
+    return result;
 };
 
 console.log(search([4,5,6,7,0,1,2], 0));
+console.log(search([3,1], 1));
+console.log(search([1,3], 1));
 console.log(search([4,5,6,7,0,1,2], 3));
 
 module.exports = {
