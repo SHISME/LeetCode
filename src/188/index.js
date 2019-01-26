93ad6631aa37bf7a513d2f860d1e0f95aee920ce
sh <475784041@qq.com>
2019-01-26 22:32:54 +0800

    init

---
+++ src/188/index.js
@@ -0,0 +1,44 @@
+/**
+ * 思路：
+ * 
+ * @param {number} k
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(k, prices) {
+    if (prices.length <= 1 || k < 1) {
+        return 0;
+    }
+    if (k >= prices.length / 2) {
+        return quickProfit(prices);
+    }
+    // const dp_sell = [];
+    // const dp_clear = [];
+    // const dp_empty = [];
+    // dp_sell[1][k] = Math.max(-prices[0], -prices[1]);
+    // dp_clear[1][k] = Math.max(0, prices[1] - prices[0]);
+    // dp_empty[1][k] = 0;
+    // for (let i = 2; i < prices.length; i++) {
+    //     dp_sell[i][k] = Math.max(dp_sell[i - 1][k], );
+    // }
+    function quickProfit(prices) {
+        let result = 0;
+        for (let i = 1; i< prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                result += prices[i] - prices[i - 1];
+            }
+        }
+        return result;
+    }
+};
+
+console.log(maxProfit(5, [1,2,4,2,5,7,2,4,9,0]));
+// console.log(maxProfit(2, [3,2,6,5,0,3,]));
+
+module.exports = {
+    title:'Best Time to Buy and Sell Stock IV',
+    id:'188',
+    difficulty:'hard',
+    url:'https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/',
+    have_md:true,
+};
