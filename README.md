7dc02d9411188fa5be48fc1f9fc36c40f761e8c8
sh <475784041@qq.com>
2019-02-14 23:12:34 +0800

    Find First and Last Position of Element in Sorted Array

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## LeetCode
 
-当前进度:84
+当前进度:85
 
 | #   | Title                                                                                                                             | javascript                | Difficulty |
 | --- | --------------------------------------------------------------------------------------------------------------------------------- | ------------------------- | ---------- |
@@ -36,6 +36,7 @@
 |31|[Next Permutation](https://leetcode.com/problems/next-permutation/description/)|[link](/src/31/index.js)|medium|
 |32|[Longest Valid Parentheses](https://leetcode.com/problems/longest-valid-parentheses/)|[link](/src/32/index.js)|hard|
 |33|[Search in Rotated Sorted Array](https://leetcode.com/problems/search-in-rotated-sorted-array/)|[link](/src/33/index.js)|medium|
+|34|[Find First and Last Position of Element in Sorted Array](https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/)|[link](/src/34/index.js)|Medium|
 |43|[ Multiply Strings](https://leetcode.com/problems/multiply-strings/)|[link](/src/43/index.md)|medium|
 |46|[Permutations](https://leetcode.com/problems/permutations/)|[link](/src/46/index.js)|medium|
 |48|[Rotate Image](https://leetcode.com/problems/rotate-image/)|[link](/src/48/index.js)|medium|
--- src/34/index.js
+++ src/34/index.js
@@ -1,40 +1,76 @@
+/*
+ * @lc app=leetcode id=34 lang=javascript
+ *
+ * [34] Find First and Last Position of Element in Sorted Array
+ *
+ * https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+ *
+ * algorithms
+ * Medium (32.84%)
+ * Total Accepted:    261.6K
+ * Total Submissions: 796.5K
+ * Testcase Example:  '[5,7,7,8,8,10]\n8'
+ *
+ * Given an array of integers nums sorted in ascending order, find the starting
+ * and ending position of a given target value.
+ * 
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ * 
+ * If the target is not found in the array, return [-1, -1].
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [5,7,7,8,8,10], target = 8
+ * Output: [3,4]
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [5,7,7,8,8,10], target = 6
+ * Output: [-1,-1]
+ * 
+ */
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-    const targetIndex = foundTargetIndex(nums, target);
-    if (targetIndex === -1) {
+    const right = foundRightIndex(nums, 0, nums.length - 1, target);
+    if (nums[right] !== target) {
         return [-1, -1];
     }
-    let left = targetIndex;
-    let right = targetIndex;
-    while (nums[left - 1] === target) {
-        left--;
-    }
-    while (nums[right + 1] === target) {
-        right++;
-    }
+    const left = foundLeftIndex(nums, 0, right, target);
     return [left, right];
-    function foundTargetIndex(nums, target) {
-        let left = 0;
-        let right = nums.length - 1;
+    function foundLeftIndex(nums, left, right, target) {
         while (left <= right) {
-            const middle = Math.floor((left + right) / 2);
-            if (nums[middle] > target) {
-                right = middle - 1;
+            const mid = Math.floor((left + right) / 2);
+            if (nums[mid] < target) {
+                left = mid + 1;
                 continue;
             }
-            if (nums[middle] < target) {
-                left = middle + 1;
-                continue;
-            }
-            return middle;
+            right = mid - 1;
         }
-        return -1;
+        return left;
+    }
+
+   function foundRightIndex(nums, left, right, target) {
+       while (left <= right) {
+           const mid = Math.floor((left + right) / 2);
+           if (nums[mid] > target) {
+               right = mid - 1;
+               continue;
+           }
+           left = mid + 1;
+        }
+        return left;
     }
 };
 
-console.log(searchRange([5,7,7,8,8,10], 8));//[3,4]
-console.log(searchRange([5,7,7,8,8,10], 6));//[-1,-1]
+module.exports = {
+    id:'34',
+    title:'Find First and Last Position of Element in Sorted Array',
+    url:'https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/',
+    difficulty:'Medium',
+}
