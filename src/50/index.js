80dd6da8fd668d36f0093ba45929535b77dfe98d
sh <475784041@qq.com>
2019-04-06 23:40:02 +0800

    init 50

---
+++ src/50/index.js
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode id=50 lang=javascript
+ *
+ * [50] Pow(x, n)
+ *
+ * https://leetcode.com/problems/powx-n/description/
+ *
+ * algorithms
+ * Medium (27.42%)
+ * Total Accepted:    303.1K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '2.00000\n10'
+ *
+ * Implement pow(x, n), which calculates x raised to the power n (xn).
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 2.00000, 10
+ * Output: 1024.00000
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 2.10000, 3
+ * Output: 9.26100
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: 2.00000, -2
+ * Output: 0.25000
+ * Explanation: 2-2 = 1/22 = 1/4 = 0.25
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * -100.0 < x < 100.0
+ * n is a 32-bit signed integer, within the range [−231, 231 − 1]
+ * 
+ * 
+ */
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    const isNegative = n < 0;
+    if (n === 0) {
+        return 1;
+    }
+    n = Math.abs(n);
+    let res = 1;
+    while (n > 0) {
+        res *= x;
+        n--;
+    }
+    if (isNegative) {
+        return 1 / res;
+    }
+    return res;
+};
+
+console.log(myPow(2.00000, -2147483648));
+
+module.exports = {
+    id:'50',
+    title:'Pow(x, n)',
+    url:'https://leetcode.com/problems/powx-n/description/',
+    difficulty:'Medium',
+}
