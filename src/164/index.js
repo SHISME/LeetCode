570f1c5bc78a61fa1e216162ef2ff4b84f61171a
sh <475784041@qq.com>
2019-04-08 23:19:53 +0800

    init 164

---
+++ src/164/index.js
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=164 lang=javascript
+ *
+ * [164] Maximum Gap
+ *
+ * https://leetcode.com/problems/maximum-gap/description/
+ *
+ * algorithms
+ * Hard (31.68%)
+ * Total Accepted:    67.7K
+ * Total Submissions: 210.8K
+ * Testcase Example:  '[3,6,9,1]'
+ *
+ * Given an unsorted array, find the maximum difference between the successive
+ * elements in its sorted form.
+ * 
+ * Return 0 if the array contains less than 2 elements.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [3,6,9,1]
+ * Output: 3
+ * Explanation: The sorted form of the array is [1,3,6,9], either
+ * (3,6) or (6,9) has the maximum difference 3.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [10]
+ * Output: 0
+ * Explanation: The array contains less than 2 elements, therefore return 0.
+ * 
+ * Note:
+ * 
+ * 
+ * You may assume all elements in the array are non-negative integers and fit
+ * in the 32-bit signed integer range.
+ * Try to solve it in linear time/space.
+ * 
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumGap = function(nums) {
+    
+};
+
+module.exports = {
+    id:'164',
+    title:'Maximum Gap',
+    url:'https://leetcode.com/problems/maximum-gap/description/',
+    difficulty:'Hard',
+}
