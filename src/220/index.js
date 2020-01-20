dc668a2608fc086bbb233a0c465f88b5b869c663
sh <475784041@qq.com>
2020-01-20 20:26:42 +0800

    5 最长回文动态规划解法

---
+++ src/220/index.js
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=220 lang=javascript
+ *
+ * [220] Contains Duplicate III
+ *
+ * https://leetcode.com/problems/contains-duplicate-iii/description/
+ *
+ * algorithms
+ * Medium (19.33%)
+ * Total Accepted:    109.1K
+ * Total Submissions: 534.7K
+ * Testcase Example:  '[1,2,3,1]\n3\n0'
+ *
+ * Given an array of integers, find out whether there are two distinct indices
+ * i and j in the array such that the absolute difference between nums[i] and
+ * nums[j] is at most t and the absolute difference between i and j is at most
+ * k.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [1,2,3,1], k = 3, t = 0
+ * Output: true
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [1,0,1,1], k = 1, t = 2
+ * Output: true
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: nums = [1,5,9,1,5,9], k = 2, t = 3
+ * Output: false
+ * 
+ * 
+ * 
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} t
+ * @return {boolean}
+ */
+var containsNearbyAlmostDuplicate = function(nums, k, t) {
+    
+};
+
+module.exports = {
+    id:'220',
+    title:'Contains Duplicate III',
+    url:'https://leetcode.com/problems/contains-duplicate-iii/description/',
+    difficulty:'Medium',
+}
--- src/5/index.js
+++ src/5/index.js
@@ -46,3 +46,35 @@ var longestPalindrome = function(s) {
     }
     return s.slice(result[0], result[1] + 1);
 };
+
+/**
+ * 解法二：
+ *
+ * 动态规划
+ *
+ * 1. 我们设dp[l][r]为字符串str[l~r]是否为回文
+ * 2. 如果str[l~r]是回文，则其左右向内向内收缩一位必是回文
+ * 3. str[(l+1) ~ (r-1)]必是回文
+ * 4. 反向推到可知，str[(l+1) ~ (r-1)]是回文，且str[l] === str[r]则 dp[l][r] = true
+ * 5. 或者 str[l] === str[r],且str[(l+1) ~ (r-1)]长度小于等于1的时候，dp[l][r]也是回文，即 r-1 - (l+1) + 1 <= 1 =>  r - l <= 2
+ *
+ *
+ * @param s
+ */
+longestPalindrome = (s) => {
+    if (!s || s.length <= 1) return s;
+    let result = [0, 0];
+    const dp = Array.from({length: s.length}, () => []);
+    for (let r = 0; r < s.length; r++) {
+        for (let l = 0; l < r; l++) {
+            if (s[l] === s[r] && (r - l <= 2 || dp[l+1][r-1])) {
+                dp[l][r] = true;
+                if (r - l > result[1] - result[0]) {
+                    result[0] = l;
+                    result[1] = r;
+                }
+            }
+        }
+    }
+    return s.substring(result[0], result[1] + 1);
+}
