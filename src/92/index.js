c7bd6bb534243925d081924afae98479207e0f16
sh <475784041@qq.com>
2020-02-23 11:06:28 +0800

    92题

---
+++ src/92/index.js
@@ -0,0 +1,95 @@
+/*
+ * @lc app=leetcode id=92 lang=javascript
+ *
+ * [92] Reverse Linked List II
+ *
+ * https://leetcode.com/problems/reverse-linked-list-ii/description/
+ *
+ * algorithms
+ * Medium (33.82%)
+ * Total Accepted:    241.1K
+ * Total Submissions: 647.6K
+ * Testcase Example:  '[1,2,3,4,5]\n2\n4'
+ *
+ * Reverse a linked list from position m to n. Do it in one-pass.
+ * 
+ * Note: 1 ≤ m ≤ n ≤ length of list.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 1->2->3->4->5->NULL, m = 2, n = 4
+ * Output: 1->4->3->2->5->NULL
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} m
+ * @param {number} n
+ * @return {ListNode}
+ */
+var reverseBetween = function(head, m, n) {
+    if (!head || !head.next) {
+        return head;
+    }
+    const result = new ListNode();
+    let resultTmp = result;
+    let reverseNodeHead;
+    while (head) {
+        const curNode = new ListNode(head.val);
+        if (m > 1) {
+            resultTmp.next = curNode;
+            resultTmp = curNode;
+            m--;
+            n--;
+        }else if (n) {
+            if (!reverseNodeHead) {
+                reverseNodeHead = curNode;
+                n--;
+            } else {
+                curNode.next = reverseNodeHead;
+                reverseNodeHead = curNode;
+                n--
+            }
+        } else {
+            if (reverseNodeHead) {
+                resultTmp.next = reverseNodeHead;
+                while (reverseNodeHead.next) {
+                    reverseNodeHead = reverseNodeHead.next
+                }
+                resultTmp = reverseNodeHead;
+                reverseNodeHead = null;
+            }
+            resultTmp.next = curNode;
+            resultTmp = curNode;
+        }
+        head = head.next;
+    }
+
+    if (reverseNodeHead) {
+      resultTmp.next = reverseNodeHead;
+    }
+    return result.next;
+};
+
+const { make_list_node, ListNode } = require('../utils');
+const node = make_list_node([1, 2, 3, 4, 5])
+let res = reverseBetween(node, 2, 4);
+while (res) {
+    console.log(res.val);
+    res = res.next;
+}
+module.exports = {
+    id:'92',
+    title:'Reverse Linked List II',
+    url:'https://leetcode.com/problems/reverse-linked-list-ii/description/',
+    difficulty:'Medium',
+}
