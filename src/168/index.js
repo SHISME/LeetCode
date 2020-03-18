1a8a6185827615f0f71b9c2700bbfef98da8fa00
sh <475784041@qq.com>
2020-03-18 08:51:58 +0800

    初始化

---
+++ src/168/index.js
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode id=168 lang=javascript
+ *
+ * [168] Excel Sheet Column Title
+ *
+ * https://leetcode.com/problems/excel-sheet-column-title/description/
+ *
+ * algorithms
+ * Easy (28.42%)
+ * Total Accepted:    202.5K
+ * Total Submissions: 669.3K
+ * Testcase Example:  '1'
+ *
+ * Given a positive integer, return its corresponding column title as appear in
+ * an Excel sheet.
+ * 
+ * For example:
+ * 
+ * 
+ * ⁠   1 -> A
+ * ⁠   2 -> B
+ * ⁠   3 -> C
+ * ⁠   ...
+ * ⁠   26 -> Z
+ * ⁠   27 -> AA
+ * ⁠   28 -> AB 
+ * ⁠   ...
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 1
+ * Output: "A"
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 28
+ * Output: "AB"
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: 701
+ * Output: "ZY"
+ * 
+ */
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var convertToTitle = function(n) {
+    const map = {
+        1:
+    }
+};
+
+module.exports = {
+    id:'168',
+    title:'Excel Sheet Column Title',
+    url:'https://leetcode.com/problems/excel-sheet-column-title/description/',
+    difficulty:'Easy',
+}
---
+++ src/171/index.js
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode id=171 lang=javascript
+ *
+ * [171] Excel Sheet Column Number
+ *
+ * https://leetcode.com/problems/excel-sheet-column-number/description/
+ *
+ * algorithms
+ * Easy (50.71%)
+ * Total Accepted:    265.2K
+ * Total Submissions: 496.7K
+ * Testcase Example:  '"A"'
+ *
+ * Given a column title as appear in an Excel sheet, return its corresponding
+ * column number.
+ * 
+ * For example:
+ * 
+ * 
+ * ⁠   A -> 1
+ * ⁠   B -> 2
+ * ⁠   C -> 3
+ * ⁠   ...
+ * ⁠   Z -> 26
+ * ⁠   AA -> 27
+ * ⁠   AB -> 28 
+ * ⁠   ...
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "A"
+ * Output: 1
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "AB"
+ * Output: 28
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "ZY"
+ * Output: 701
+ * 
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var titleToNumber = function(s) {
+    
+};
+
+module.exports = {
+    id:'171',
+    title:'Excel Sheet Column Number',
+    url:'https://leetcode.com/problems/excel-sheet-column-number/description/',
+    difficulty:'Easy',
+}
