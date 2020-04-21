eb390386bb7713e7a06a13e68e2aecf4a16639f0
sh <475784041@qq.com>
2020-04-21 22:22:26 +0800

    101

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 | Easy   | Medium   | Hard   |
 | ------ | -------- | ------ |
-| 39 | 87 | 36 |
+| 40 | 87 | 36 |
 
 ### 答案
 
@@ -93,6 +93,7 @@
 |98|[Validate Binary Search Tree](https://leetcode.com/problems/validate-binary-search-tree/description/)|[link](/src/98/index.js)|Medium|
 |99|[Recover Binary Search Tree](https://leetcode.com/problems/recover-binary-search-tree/description/)|[link](/src/99/index.js)|Hard|
 |100|[Same Tree](https://leetcode.com/problems/same-tree/description/)|[link](/src/100/index.js)|Easy|
+|101|[Symmetric Tree](https://leetcode.com/problems/symmetric-tree/description/)|[link](/src/101/index.js)|Easy|
 |102|[Binary Tree Level Order Traversal](https://leetcode.com/problems/binary-tree-level-order-traversal/description/)|[link](/src/102/index.js)|Medium|
 |103|[Binary Tree Zigzag Level Order Traversal](https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/)|[link](/src/103/index.js)|Medium|
 |104|[ Maximum Depth of Binary Tree](https://leetcode.com/problems/maximum-depth-of-binary-tree/solution/)|[link](/src/104/index.js)|easy|
---
+++ src/101/index.js
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=101 lang=javascript
+ *
+ * [101] Symmetric Tree
+ *
+ * https://leetcode.com/problems/symmetric-tree/description/
+ *
+ * algorithms
+ * Easy (42.50%)
+ * Total Accepted:    588.6K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[1,2,2,3,4,4,3]'
+ *
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric
+ * around its center).
+ * 
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ * 
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠/ \ / \
+ * 3  4 4  3
+ * 
+ * 
+ * 
+ * 
+ * But the following [1,2,2,null,3,null,3] is not:
+ * 
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠  \   \
+ * ⁠  3    3
+ * 
+ * 
+ * 
+ * 
+ * Follow up: Solve it both recursively and iteratively.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    const isSymmetricHelp = (root1, root2) => {
+        if (!root1 && !root2) return true;
+        if (!root1 || !root2) return false;
+        if (root1.val !== root2.val) return false;
+        return isSymmetricHelp(root1.left, root2.right) && isSymmetricHelp(root1.right, root2.left)
+    };
+    return isSymmetricHelp(root, root)
+};
+
+module.exports = {
+    id:'101',
+    title:'Symmetric Tree',
+    url:'https://leetcode.com/problems/symmetric-tree/description/',
+    difficulty:'Easy',
+}
