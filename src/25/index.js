001efcc971b76f518fc29cb74c870c79c9689375
sh <475784041@qq.com>
2021-10-26 23:43:25 +0800

    new 25

--- src/25/index.js
+++ src/25/index.js
@@ -2,17 +2,17 @@ const { ListNode, make_list_node } = require('../utils/');
 
 /**
  * 思路：
- * 
+ *
  * 1.将分成 k 个一组装进一个数组中
  * 2.从后往前遍历数组
  * 3.再把组数组reverse
  * 4.返回第一组的最后一项
- * 
+ *
  * @param {ListNode} head
  * @param {number} k
  * @return {ListNode}
  */
-var reverseKGroup = function (head, k) {
+var reverseKGroup1 = function (head, k) {
     if (k === 1) {
         return head;
     }
@@ -55,61 +55,50 @@ var reverseKGroup = function (head, k) {
 
 /**
  * 思路：
- * 
+ *
  * 参考了一些别人的答案，感觉思路更清晰简单一些，这里试着自己实现一遍
- * 
+ *
  * 这里用了递归的思想，我们当前的最后最后一项需要知道下一个reverse的第一项，
- * 
+ *
  * 每k个我们自己做一次reverse
- * 
+ *
  * 1.现获取后面的第一项 temp_node
  * 2.缓存head 的 下一项 next_node
  * 3.head.next 指向 temp_node
  * 4.temp_node = head
  * 5.head = next_node
- * 
+ *
  * @param {ListNode} head
  * @param {number} k
  * @return {ListNode}
  */
 var reverseKGroup = function (head, k) {
-    let cur = head;
-    let i = 1;
-    while (cur && i < k) {
-        cur = cur.next;
-        i++;
+    if (k === 1) {
+        return head;
     }
-    if (i === k) {
-        let temp_node = reverseKGroup(curr, k);
-        while (i > 0) {
-            let next_node = head.next;
-            head.next = temp_node;
-            temp_node = head;
-            head = next_node;
-            i--;
-        }
-    }
+   let tempNode = head;
+   let i = 1;
+   while (i < k && tempNode) {
+       tempNode = tempNode.next;
+       i++;
+   }
+   if (i === k && tempNode) {
+       tempNode = reverseKGroup(tempNode.next, k);
+       while (i > 0) {
+           const next= head.next;
+           head.next = tempNode;
+           tempNode = head;
+           head = next;
+           i--;
+       }
+       return tempNode;
+   }
+   return head;
 }
 
 let test = make_list_node([1, 2, 3, 4,5]);
-test = reverseKGroup(test, 3);
-console.log(test);
+test = reverseKGroup(test, 1);
 while (test) {
     console.log(test.val);
     test = test.next;
-}
-
-/**
- * 
- * @param {ListNode} head 
- */
-function reverseNode(head) {
-    let tempNode = null;
-    while (head.next) {
-        let nextNode = head.next;
-        head.next = tempNode;
-        tempNode = head;
-        head = nextNode;
-    }
-    head.next = tempNode;
 }
